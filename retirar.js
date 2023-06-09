3d04c3dceb49172dae85c02532cf5b313aed9fcf
Eric Cristo <ericcristo2@gmail.com>
2023-06-09 16:53:35 +0100

    modificando arrays

--- retirar.js
+++ retirar.js
@@ -5,3 +5,22 @@ notas.pop();
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(`A média é ${media}`);
+
+const arrayVazia = [,,,];
+console.log(arrayVazia.length);
+console.log(arrayVazia[0]);
+console.log(arrayVazia[1]);
+console.log(arrayVazia[2]);
+
+const arrayVazia2 = [,,,];
+console.log(arrayVazia.length);
+arrayVazia.push(50);
+console.log(arrayVazia);
+console.log(arrayVazia.length);
+
+// teste de mistura de arrays
+
+const strings = ["eric", "vitoria"];
+                                                                                                // funciona
+const testeDeStringsENumerais = ((notas[0] + notas[1] + notas[2] + notas[3]) / notas.length) + ` é a média das notas de ${strings[0]}`; 
+console.log(testeDeStringsENumerais);
