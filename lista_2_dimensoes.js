0c14a85f4c6cf67171f5b53b1e6bc74506b3a448
Eric Cristo <ericcristo2@gmail.com>
2023-06-17 17:03:19 +0100

    Utilizando 2 dimensoes de uma lista

---
+++ lista_2_dimensoes.js
@@ -0,0 +1,9 @@
+const alunos = ["joao", "ju", "vi", "pedro"]
+const notas = [10, 8, 7.5, 9];
+
+const listaAlunosEMedias = [alunos, notas];
+
+                                                                                //posicao da primeira parte da lista --> alunos
+console.log(`A aluna da primeira posição da lista de alunos é ${listaAlunosEMedias[0][1]}
+ a nota dessa aluna é ${listaAlunosEMedias[1][1]}`)
+                                                                                    //posicao sa sting dentro da primeira lista ou seja os alunos em sim
