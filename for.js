5cff6ba148a32704e62ac016cf26559376264127
Eric Cristo <ericcristo2@gmail.com>
2023-06-19 00:27:53 +0100

    for arrays

--- for.js
+++ for.js
@@ -5,7 +5,16 @@ const numeros = [100, 200, 300, 400, 500, 600];
 // terceira expressao: sempre executada ao final do bloco
 
 
-for (let indice = 0; indice < numeros.length; indice++) {
-    console.log(numeros[indice]);
+//for (let indice = 0; indice < numeros.length; indice++) {
+//    console.log(numeros[indice]);
 
-}
+//}
+
+for (let indice = 15; indice >= 10; indice--) {
+    console.log(indice);
+  }
+
+  //infinito (bug)
+ // for (let indice = 0; indice <= 10; ) {
+ //   console.log(indice);
+ // }
