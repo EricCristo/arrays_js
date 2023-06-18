c33e9912744a6308bcbd0ba5bb146cf0649d641a
Eric Cristo <ericcristo2@gmail.com>
2023-06-19 00:50:22 +0100

    somando com itens de arrays com for

--- media-for.js
+++ media-for.js
@@ -11,3 +11,21 @@ for (let i = 0; i < notas.length; i++){
 const media = somaDasNotas / notas.length;
 
 console.log(`A média das notas é ${media}`);
+
+const notas1 = [10 , 6.5, 8 ,7.5]
+const notas2 = [9  , 6  , 6]
+const notas3 = [8.5, 9.5]
+
+const notasGerais = [notas1,notas2,notas3]
+
+let medias = 0
+
+for (let i = 0; i < notasGerais.length; i++) {
+    for (let j = 0; j < notasGerais[i].length; j++) {
+      medias += notasGerais[i][j]/notasGerais[i].length;
+    }
+  }
+
+  medias = medias/notasGerais.length;
+  console.log(medias)
+
