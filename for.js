857efe0c5cb86e243bfc85fb0d9ed36f95814099
Eric Cristo <ericcristo2@gmail.com>
2023-06-19 00:20:24 +0100

    laços de repetição

---
+++ for.js
@@ -0,0 +1,11 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+
+// primeira parte: executada apenas uma unica vez
+// segunda expressao: condicao de execucao
+// terceira expressao: sempre executada ao final do bloco
+
+
+for (let indice = 0; indice < numeros.length; indice++) {
+    console.log(numeros[indice]);
+
+}
