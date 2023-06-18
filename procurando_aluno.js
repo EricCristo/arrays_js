419cf50b3f83ac74070f0ce6914114aec11ffa3d
Eric Cristo <ericcristo2@gmail.com>
2023-06-19 00:10:24 +0100

    procurando em arrays

--- procurando_aluno.js
+++ procurando_aluno.js
@@ -1,20 +1,26 @@
 const alunos = ["joao", "ju", "vi", "pedro"]
-const notas = [10, 8, 7.5, 9];
+const media = [10, 8, 7.5, 9];
 
-const listaAlunosEMedias = [alunos, notas];
+const listaAlunosEMedias = [alunos, media];
 
 function showAlunosENotas(aluno) {
     if (listaAlunosEMedias[0].includes(aluno)){
-    //    console.log(`${aluno} está cadastrado!`);
 
-        const indice = listaAlunosEMedias[0].indexOf(aluno);
+        //const alunos = listaAlunosEMedias[0];
+        //const medias = listaAlunosEMedias[1];
+        const [alunos, media] = listaAlunosEMedias;
+        
+
+        //    console.log(`${aluno} está cadastrado!`);
+
+        const indice = alunos.indexOf(aluno);
 
      //   console.log(indice);
 
-        const media = listaAlunosEMedias[1][indice]
+        const medias = media[indice];
 
     //    console.log(media);
-        console.log(`${aluno} tem a média ${media}` )
+        console.log(`${aluno} tem a média ${medias}` )
 
 } else {
 
