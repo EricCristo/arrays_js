503ee5301f48bca855cc4bfe093667a785380897
Eric Cristo <ericcristo2@gmail.com>
2023-06-19 01:15:51 +0100

    Includes e indexOf

--- procurando_aluno.js
+++ procurando_aluno.js
@@ -3,6 +3,7 @@ const media = [10, 8, 7.5, 9];
 
 const listaAlunosEMedias = [alunos, media];
 
+//O includes() retorna true ou false, dependendo se o valor passado como parâmetro está presente ou não no array. O indexOf() retorna a posição no array do valor passado como parâmetro. Utilizamos esses dois métodos em conjunto para primeiro verificar se um aluno estava presente em uma lista, e depois obter sua posição para buscar sua nota em outra lista.
 function showAlunosENotas(aluno) {
     if (listaAlunosEMedias[0].includes(aluno)){
 
