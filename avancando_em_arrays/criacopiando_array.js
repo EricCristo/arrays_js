4fef7b15344912ed0470efc44d3b29173a49db2e
Eric Cristo <ericcristo2@gmail.com>
2023-06-21 17:32:38 +0100

    spread operator

--- avancando_em_arrays/criacopiando_array.js
+++ avancando_em_arrays/criacopiando_array.js
@@ -6,3 +6,21 @@ novasNotas.push(10);
 
 console.log(`As novas notas sâo ${novasNotas}`);
 console.log(`As notas originais sâo ${notas}`);
+
+let num1 = 10;
+let num2 = num1;
+
+num2 += 5;
+num1 += 1;
+
+console.log(`Num1 é ${num1}. Num2 é ${num2}`);
+
+let numeroOriginal = 10;
+
+function alteraNumero(numero) {
+  numero = 50;
+
+  console.log(`numero do parâmetro é ${numero}. numeroOriginal é ${numeroOriginal}`);
+}
+
+alteraNumero(numeroOriginal);
